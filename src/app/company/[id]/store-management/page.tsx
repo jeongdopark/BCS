cafb984f323691e64165a9a96e576fd92b0fd42d
Xeraph <jeongdo.dev@gmail.com>
2024-03-19 17:08:31 +0900

    Rename: store -> store-mangement

--- src/app/company/[id]/store-management/page.tsx
+++ src/app/company/[id]/store-management/page.tsx
@@ -1,7 +1,7 @@
 import Header from '@/components/store/Header'
 import StoreTable from '@/components/store/StoreTable'
 
-export default function Store() {
+export default function StoreManagement() {
   return (
     <div className="w-full flex justify-center">
       <div className="w-[65%] mt-[60px] flex flex-col gap-5 ">
