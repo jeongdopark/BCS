6f4855200db706d469a53f840d7a2ab028610dbd
Xeraph <jeongdo.dev@gmail.com>
2024-03-29 17:01:29 +0900

    Feat: 상품 옵션 컴포넌트

--- src/components/order/CheckTypeOption.tsx
+++ src/components/order/CheckTypeOption.tsx
@@ -1,15 +1,38 @@
 import { Checkbox } from '@/components/ui/checkbox'
 import { OptionSelectType, OptionBooleanType } from '@/types/product'
 
-const CheckTypeOption = ({ option }: { option: OptionBooleanType }) => {
+const CheckTypeOption = ({
+  option,
+  handleOptionChange,
+}: {
+  option: OptionBooleanType
+  handleOptionChange: ({
+    option,
+    price,
+    name,
+  }: {
+    option: string
+    price: number
+    name: string
+  }) => void
+}) => {
   return (
     <div className="flex items-center space-x-2">
-      <Checkbox id="terms" />
+      <Checkbox
+        id="terms"
+        onClick={() =>
+          handleOptionChange({
+            option: option.options[0].name,
+            name: option.name,
+            price: option.options[0].price,
+          })
+        }
+      />
       <label
         htmlFor="terms"
         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
       >
-        샷 추가
+        {option.name} +{option.options[0].price}
       </label>
     </div>
   )
--- src/components/order/TabTypeOption.tsx
+++ src/components/order/TabTypeOption.tsx
@@ -1,12 +1,42 @@
+'use client'
 import { OptionSelectType } from '@/types/product'
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
-const TabTypeOption = ({ option }: { option: OptionSelectType }) => {
+const TabTypeOption = ({
+  option,
+  handleOptionChange,
+}: {
+  option: OptionSelectType
+  handleOptionChange: ({
+    option,
+    name,
+    price,
+  }: {
+    name: string
+    option: string
+    price: number
+  }) => void
+}) => {
   return (
-    <Tabs defaultValue={option.options[0].name} className="w-[400px]">
+    <Tabs
+      defaultValue={option.options[0].name}
+      className="w-[400px]"
+      onValueChange={(e) => console.log(e)}
+    >
       <TabsList>
         {option.options.map((elem) => (
-          <TabsTrigger value={elem.name}>
+          <TabsTrigger
+            value={JSON.stringify({ name: elem.name, price: elem.price })}
+            onClick={() => {
+              console.log('click')
+
+              handleOptionChange({
+                name: option.name,
+                option: elem.name,
+                price: elem.price,
+              })
+            }}
+          >
             {elem.name} +{elem.price}
           </TabsTrigger>
         ))}
