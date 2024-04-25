7ad6ff69c250c993e53d1a4320b4c2c0f896cd8a
Xeraph <jeongdo.dev@gmail.com>
2024-04-25 16:30:28 +0900

    Chore: react-table 제거

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,5 +1,3 @@
-import { ColumnDef } from '@tanstack/react-table'
-
 export const PAGINATION = {
   CATEGORY: 5,
   PRODUCT: 4,
@@ -25,22 +23,3 @@ export type Payment = {
   time: string
   payment: 'toss' | 'kakao'
 }
-
-export const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: 'UID',
-    header: 'Order UID',
-  },
-  {
-    accessorKey: 'price',
-    header: 'Price',
-  },
-  {
-    accessorKey: 'payment',
-    header: 'Payment',
-  },
-  {
-    accessorKey: 'time',
-    header: 'Time',
-  },
-]
